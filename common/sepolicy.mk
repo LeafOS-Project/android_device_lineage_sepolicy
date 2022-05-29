56263406c29e6a0baa47dd62160dfe748a2dddb1
Tim Zimmermann <tim@linux4.de>
2022-05-29 05:54:55 +0200

    Initial stripped down import of lineage sepolicy
    * From https://github.com/LineageOS/android_device_lineage_sepolicy/tree/2819ec79d749830aa4c9027f01228cd86b3eb136
    * Removed most stuff not related to lineage HALs
    
    Change-Id: I23e6fa2bad80ad43cb62b4b5f1de427b5cb33078

---
+++ common/sepolicy.mk
@@ -0,0 +1,10 @@
+#
+# This policy configuration will be used by all products that
+# inherit from Lineage
+#
+
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
+    device/lineage/sepolicy/common/public
+
+BOARD_VENDOR_SEPOLICY_DIRS += \
+    device/lineage/sepolicy/common/vendor
---
+++ exynos/sepolicy.mk
@@ -0,0 +1,7 @@
+#
+# This policy configuration will be used by all exynos products
+# that inherit from Lineage
+#
+
+BOARD_VENDOR_SEPOLICY_DIRS += \
+    device/lineage/sepolicy/exynos/vendor
